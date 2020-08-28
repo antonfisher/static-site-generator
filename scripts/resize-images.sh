d712b20f444f06b9b3127660d4b6d32a11e141e2
Anton Fisher <a.fschr@gmail.com>
2020-08-27 22:18:14 -0700

    feat: image script to expect max_width argument

--- scripts/resize-images.sh
+++ scripts/resize-images.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 
-MAX_WIDTH=800
+#MAX_WIDTH=730
 
-echo "Looking for images..."
+MAX_WIDTH="${1}"
+re='^[0-9]+$'
+if ! [[ "$MAX_WIDTH" =~ $re ]] ; then
+  echo "Usage:" >&2;
+  echo "    resize-images.sh MAX_WIDTH" >&2;
+  echo "" >&2;
+  echo "    MAX_WIDTH is a number greater than 0" >&2;
+  exit 1;
+elif [ "$MAX_WIDTH" -le "0" ]; then
+  echo "ERROR: MAX_WIDTH cannot be less than 1" >&2;
+  exit 1;
+fi
+
+echo "Looking for images in: `pwd` ..."
 
 find . -name "*.png" -o -name "*.jpg" -o -name "*.jpeg" |
   while read name; do
