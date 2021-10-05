7943b3c3b3ee562e2bcb9e4f321133513d031019
Anton Fisher <a.fschr@gmail.com>
2021-10-04 23:33:35 -0700

    chore: image resize script: add option to ignore files

--- scripts/resize-images.sh
+++ scripts/resize-images.sh
@@ -11,14 +11,18 @@
 #   apt install imagemagick pngquant
 #
 
-#MAX_WIDTH=730
+#MAX_WIDTH=790
 MAX_WIDTH="${1}"
 
 if ! [[ "$MAX_WIDTH" =~ ^[0-9]+$ ]]; then
   echo "Usage:" >&2
-  echo "    resize-images.sh MAX_WIDTH" >&2
+  echo "    resize-images.sh MAX_WIDTH"
   echo "" >&2
-  echo "    MAX_WIDTH is a number greater than 0" >&2
+  echo "    MAX_WIDTH is a number greater than 0 (790 for antonfisher.com)" >&2
+  echo "" >&2
+  echo "Filename rules:" >&2
+  echo "   *-ir.* ignore resizing" >&2
+  echo "   *-ic.* ignore compression" >&2
   exit 1
 elif [ "$MAX_WIDTH" -le "0" ]; then
   echo "ERROR: MAX_WIDTH cannot be less than 1" >&2
@@ -31,13 +35,19 @@ find . -name "*.png" -o -name "*.jpg" -o -name "*.jpeg" |
   while read name; do
     width=$(identify -format "%w" "$name")
     if [ "$width" -gt "$MAX_WIDTH" ]; then
-      convert ${name} -verbose -resize $MAX_WIDTH ${name}
-      if [[ "$name" == *\.png ]]; then
-        size_before=$(ls -hal ${name} | cut -d ' ' -f 5)
-        pngquant --force --speed 1 --skip-if-larger --ext .png ${name}
-        size_after=$(ls -hal ${name} | cut -d ' ' -f 5)
-        echo -e "Compress: ${size_before}\t-> ${size_after}"
+      size_before=$(ls -lha ${name} | awk '{print $5}')
+      if [[ "$name" =~ -ir\.*.+$ ]]; then
+        echo -e "Resize: IGNORED"
+      else
+        convert ${name} -verbose -resize $MAX_WIDTH ${name}
       fi
+      if [[ "$name" =~ -ic\.*.+$ ]]; then
+        echo -e "Compress: IGNORED"
+      elif [[ "$name" =~ \.png$ ]]; then
+        pngquant --force --speed 1 --skip-if-larger --ext .png ${name}
+      fi
+      size_after=$(ls -lha ${name} | awk '{print $5}')
+      echo -e "Size: ${size_before}\t-> ${size_after}"
     fi
   done
 
